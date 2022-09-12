9e110506e745d7cfb047203307854065d113b298
define16 <radix7318@gmail.com>
2022-09-12 20:49:27 +0900

    Add model tutorial

---
+++ model/idex.py
@@ -0,0 +1,20 @@
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from model.neural_network import NeuralNetwork
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+model = NeuralNetwork().to(device)
+print(model)
+
+
+X = torch.rand(1, 28, 28, device=device)
+logits = model(X)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
